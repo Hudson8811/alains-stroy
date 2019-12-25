235d3248e08aa8c794560a38509a2766040ea2d4
PipZip <37911683+PipZip@users.noreply.github.com>
2019-12-25 20:13:53 +0600

    news

--- build/js/pages/news-page.js
+++ build/js/pages/news-page.js
@@ -6,4 +6,14 @@ $('.slider-news').owlCarousel({
     navText: false,
     dots: true,
     items: 1,
+    onInitialized  : counter,
+    onTranslated : counter,
 });
+
+function counter(event) {
+    var totalItems = $('.owl-item:not(.cloned)' ).length;
+    var currentIndex = $('div.active').index() + 1 ;
+
+    $('.current-slide').html(currentIndex);
+    $('.total-slide').html(totalItems);
+}
